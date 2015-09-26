5a84e6e03a762a62e5aa0174f9885e39ff5b82c2
Fofir <ofir.weinstock@gmail.com>
2015-09-26 14:00:42 +0200

    Added better state handling to the rule form

--- src/components/ruleform.component.js
+++ src/components/ruleform.component.js
@@ -27,19 +27,18 @@ export default React.createClass(
         }
     },
 
-
-    _onAddClick()
+    _onChange( e )
     {
-        var rule =
-        {
-            id            : this.refs.id.getValue(),
-            title         : this.refs.title.getValue(),
-            body          : this.refs.body.getValue(),
-            trueResultId  : this.refs.trueResultId.getValue(),
-            falseResultId : this.refs.falseResultId.getValue()
-        }
+        var stateToSet = {};
+        stateToSet[e.target.name] = e.target.value;
+        this.setState( stateToSet );
+    },
 
-        addRule( rule );
+
+    _onAddClick( e )
+    {
+        e.preventDefault();
+        addRule( this.state );
     },
 
 
@@ -53,11 +52,11 @@ export default React.createClass(
                     <h2 className="page-header">Step 1: Create a new rule! <small>Don't be shy!</small></h2>
                     {displayAlert}
                     <form>
-                        <Input type="text" ref="id" label="Rule id" placeholder="e.g. 12" />
-                        <Input type="text" ref="title" label="Rule title" placeholder="e.g. Check if the data is relly there" />
-                        <Input type="textarea" ref="body" label="Rule body" placeholder="e.g. function( data ) { returns data !== false }" />
-                        <Input type="text" ref="trueResultId" label="If result is true - go to this rule" placeholder="e.g. 233" />
-                        <Input type="text" ref="falseResultId" label="if result is false - go to that rule" placeholder="e.g. 342" />
+                        <Input onChange={this._onChange} type="text" name="id" label="Rule id" placeholder="e.g. 12" value={this.state.id} />
+                        <Input onChange={this._onChange} type="text" name="title" label="Rule title" placeholder="e.g. Check if the data is relly there" value={this.state.title} />
+                        <Input onChange={this._onChange} type="textarea" name="body" label="Rule body" placeholder="e.g. function( data ) { returns data !== false }" value={this.state.body} />
+                        <Input onChange={this._onChange} type="text" name="trueResultId" label="If result is true - go to this rule" placeholder="e.g. 233" value={this.state.trueResultId} />
+                        <Input onChange={this._onChange} type="text" name="falseResultId" label="if result is false - go to that rule" placeholder="e.g. 342" value={this.state.falseResultId} />
                         <ButtonInput type="submit" onClick={this._onAddClick}value="Add the rule!" bsSize="large" />
                     </form>
                 </div>
