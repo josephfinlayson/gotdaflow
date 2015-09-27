6a915e7a6d9adbfec7150c072b0147e4ee6c81fb
Fofir <ofir.weinstock@gmail.com>
2015-09-27 17:17:25 +0200

    Added documentation to the flow store

--- src/stores/flow.store.js
+++ src/stores/flow.store.js
@@ -3,6 +3,11 @@ import Dispatcher from '../dispatcher';
 import {EventEmitter} from 'events';
 import FlowGenerator from '../utils/flow-generator';
 
+/**
+ * A rule flow
+ *
+ * @var     obj
+ */
 var _flow =
 {
     title      : '',
@@ -10,39 +15,72 @@ var _flow =
     results    : []
 }
 
+
+/** 
+ * Flow store
+ *
+ * @constructor
+ */
 class FlowStore extends EventEmitter
 {
+
+    /** 
+     * Initializes the flow store
+     *
+     * @return  void
+     */
     constructor()
     {
         super();
     }
 
+
+    /** 
+     * Returns the flow object
+     *
+     * @return  obj                                     flow data
+     */
     getFlow()
     {
         return _flow;;
     }
 
+
+    /** 
+     * Emits the change event
+     *
+     * @return  void
+     */
     emitChange()
     {
         this.emit( CHANGE_EVENT );
     }
 
 
+    /** 
+     * Registers a change lstener in the flow
+     *
+     * @param   func                callback            function to run when
+     *                                                  the change event is
+     *                                                  triggered
+     *
+     * @return  void
+     */
     addChangeListener( callback )
     {
         this.on( CHANGE_EVENT, callback);
     }
 
 
-    removeChangeListener( callback )
-    {
-        this.removeListener( CHANGE_EVENT, callback );
-    }
 }
 
 
+/**
+ * Registering the flow store to event coming from the event dispatcher
+ */
 FlowStore.dispatchToken = Dispatcher.register( function( payload )
 {
+    
     var action = payload.type;
 
     switch( action )
@@ -60,10 +98,10 @@ FlowStore.dispatchToken = Dispatcher.register( function( payload )
         break;
 
         default:
-        // do nothing
     }
 
 } );
+
 var flowStore = new FlowStore();
 
 export default flowStore;
